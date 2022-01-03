033947910949766f417b8d7f2ac0defa16e3f808
ipax77 <ph.ilipp@web.de>
2022-01-03 21:42:51 +0100

    Eval ChartScore

--- src/pax.chess/Evaluation.cs
+++ src/pax.chess/Evaluation.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Text.Json.Serialization;
 
 namespace pax.chess;
 public record Evaluation
@@ -6,8 +7,21 @@ public record Evaluation
     public int Score { get; init; }
     public int Mate { get; init; }
     public bool IsBlack { get; init; }
+    public double ChartScore() => (Score, Mate) switch
+    {
+        (_, > 0) => 20.0,
+        (_, < 0) => -20.0,
+        ( >= 0, 0) => Math.Min(20, Math.Round((double)Score / 100.0, 2)),
+        ( < 0, 0) => Math.Max(-20, Math.Round((double)Score / 100.0, 2)),
+    };
 
     public override string ToString() => Mate != 0 ? $"#M{Mate}" : ((double)Score / 100.0).ToString("N2", CultureInfo.InvariantCulture);
+
+    [JsonConstructor]
+    public Evaluation()
+    {
+
+    }
 
     public Evaluation(int score, int mate, bool isBlack)
     {
