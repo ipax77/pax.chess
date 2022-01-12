bde3b7a8310eef846ff22bf84356fc85ab46d8d0
ipax77 <ph.ilipp@web.de>
2022-01-12 11:15:59 +0100

    movequality

--- src/pax.chess/Variation.cs
+++ src/pax.chess/Variation.cs
@@ -8,6 +8,7 @@ public record Variation
     public Variation? RootVariation { get; set; }
     public List<Variation> ChildVariations { get; set; } = new List<Variation>();
     public Evaluation? Evaluation { get; set; }
+    public int Pv { get; set; }
     public Variation(int startMove, Variation? rootVariation = null, int rootStartMove = 0)
     {
         StartMove = startMove;
